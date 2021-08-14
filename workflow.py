062b5c27a89896e0c51e31fa322dc324697efb0b
Ryan Tanaka <tanaka@isi.edu>
2021-08-13 18:27:06 -0700

    moved rc into data split func; lfns are now prepended with train/val/test

--- workflow.py
+++ workflow.py
@@ -13,8 +13,8 @@ import numpy as np
 
 from Pegasus.api import *
 
-log.basicConfig(level=log.DEBUG)
-IGNORE_IMAGES = ['CHNCXR_0025_0.png', 'CHNCXR_0036_0.png', 'CHNCXR_0037_0.png', 'CHNCXR_0038_0.png', 'CHNCXR_0039_0.png', 'CHNCXR_0040_0.png', 'CHNCXR_0065_0.png', 'CHNCXR_0181_0.png', 'CHNCXR_0182_0.png', 'CHNCXR_0183_0.png', 'CHNCXR_0184_0.png', 'CHNCXR_0185_0.png', 'CHNCXR_0186_0.png', 'CHNCXR_0187_0.png', 'CHNCXR_0188_0.png', 'CHNCXR_0189_0.png', 'CHNCXR_0190_0.png', 'CHNCXR_0191_0.png', 'CHNCXR_0192_0.png', 'CHNCXR_0193_0.png', 'CHNCXR_0194_0.png', 'CHNCXR_0195_0.png', 'CHNCXR_0196_0.png', 'CHNCXR_0197_0.png', 'CHNCXR_0198_0.png', 'CHNCXR_0199_0.png', 'CHNCXR_0200_0.png', 'CHNCXR_0201_0.png', 'CHNCXR_0202_0.png', 'CHNCXR_0203_0.png', 'CHNCXR_0204_0.png', 'CHNCXR_0205_0.png', 'CHNCXR_0206_0.png', 'CHNCXR_0207_0.png', 'CHNCXR_0208_0.png', 'CHNCXR_0209_0.png', 'CHNCXR_0210_0.png', 'CHNCXR_0211_0.png', 'CHNCXR_0212_0.png', 'CHNCXR_0213_0.png', 'CHNCXR_0214_0.png', 'CHNCXR_0215_0.png', 'CHNCXR_0216_0.png', 'CHNCXR_0217_0.png', 'CHNCXR_0218_0.png', 'CHNCXR_0219_0.png', 'CHNCXR_0220_0.png', 'CHNCXR_0336_1.png', 'CHNCXR_0341_1.png', 'CHNCXR_0342_1.png', 'CHNCXR_0343_1.png', 'CHNCXR_0344_1.png', 'CHNCXR_0345_1.png', 'CHNCXR_0346_1.png', 'CHNCXR_0347_1.png', 'CHNCXR_0348_1.png', 'CHNCXR_0349_1.png', 'CHNCXR_0350_1.png', 'CHNCXR_0351_1.png', 'CHNCXR_0352_1.png', 'CHNCXR_0353_1.png', 'CHNCXR_0354_1.png', 'CHNCXR_0355_1.png', 'CHNCXR_0356_1.png', 'CHNCXR_0357_1.png', 'CHNCXR_0358_1.png', 'CHNCXR_0359_1.png', 'CHNCXR_0360_1.png', 'CHNCXR_0481_1.png', 'CHNCXR_0482_1.png', 'CHNCXR_0483_1.png', 'CHNCXR_0484_1.png', 'CHNCXR_0485_1.png', 'CHNCXR_0486_1.png', 'CHNCXR_0487_1.png', 'CHNCXR_0488_1.png', 'CHNCXR_0489_1.png', 'CHNCXR_0490_1.png', 'CHNCXR_0491_1.png', 'CHNCXR_0492_1.png', 'CHNCXR_0493_1.png', 'CHNCXR_0494_1.png', 'CHNCXR_0495_1.png', 'CHNCXR_0496_1.png', 'CHNCXR_0497_1.png', 'CHNCXR_0498_1.png', 'CHNCXR_0499_1.png', 'CHNCXR_0500_1.png', 'CHNCXR_0502_1.png', 'CHNCXR_0505_1.png', 'CHNCXR_0560_1.png', 'CHNCXR_0561_1.png', 'CHNCXR_0562_1.png', 'CHNCXR_0563_1.png', 'CHNCXR_0564_1.png', 'CHNCXR_0565_1.png']
+log.basicConfig(level=log.INFO)
+IGNORE_IMAGES = {'CHNCXR_0025_0.png', 'CHNCXR_0036_0.png', 'CHNCXR_0037_0.png', 'CHNCXR_0038_0.png', 'CHNCXR_0039_0.png', 'CHNCXR_0040_0.png', 'CHNCXR_0065_0.png', 'CHNCXR_0181_0.png', 'CHNCXR_0182_0.png', 'CHNCXR_0183_0.png', 'CHNCXR_0184_0.png', 'CHNCXR_0185_0.png', 'CHNCXR_0186_0.png', 'CHNCXR_0187_0.png', 'CHNCXR_0188_0.png', 'CHNCXR_0189_0.png', 'CHNCXR_0190_0.png', 'CHNCXR_0191_0.png', 'CHNCXR_0192_0.png', 'CHNCXR_0193_0.png', 'CHNCXR_0194_0.png', 'CHNCXR_0195_0.png', 'CHNCXR_0196_0.png', 'CHNCXR_0197_0.png', 'CHNCXR_0198_0.png', 'CHNCXR_0199_0.png', 'CHNCXR_0200_0.png', 'CHNCXR_0201_0.png', 'CHNCXR_0202_0.png', 'CHNCXR_0203_0.png', 'CHNCXR_0204_0.png', 'CHNCXR_0205_0.png', 'CHNCXR_0206_0.png', 'CHNCXR_0207_0.png', 'CHNCXR_0208_0.png', 'CHNCXR_0209_0.png', 'CHNCXR_0210_0.png', 'CHNCXR_0211_0.png', 'CHNCXR_0212_0.png', 'CHNCXR_0213_0.png', 'CHNCXR_0214_0.png', 'CHNCXR_0215_0.png', 'CHNCXR_0216_0.png', 'CHNCXR_0217_0.png', 'CHNCXR_0218_0.png', 'CHNCXR_0219_0.png', 'CHNCXR_0220_0.png', 'CHNCXR_0336_1.png', 'CHNCXR_0341_1.png', 'CHNCXR_0342_1.png', 'CHNCXR_0343_1.png', 'CHNCXR_0344_1.png', 'CHNCXR_0345_1.png', 'CHNCXR_0346_1.png', 'CHNCXR_0347_1.png', 'CHNCXR_0348_1.png', 'CHNCXR_0349_1.png', 'CHNCXR_0350_1.png', 'CHNCXR_0351_1.png', 'CHNCXR_0352_1.png', 'CHNCXR_0353_1.png', 'CHNCXR_0354_1.png', 'CHNCXR_0355_1.png', 'CHNCXR_0356_1.png', 'CHNCXR_0357_1.png', 'CHNCXR_0358_1.png', 'CHNCXR_0359_1.png', 'CHNCXR_0360_1.png', 'CHNCXR_0481_1.png', 'CHNCXR_0482_1.png', 'CHNCXR_0483_1.png', 'CHNCXR_0484_1.png', 'CHNCXR_0485_1.png', 'CHNCXR_0486_1.png', 'CHNCXR_0487_1.png', 'CHNCXR_0488_1.png', 'CHNCXR_0489_1.png', 'CHNCXR_0490_1.png', 'CHNCXR_0491_1.png', 'CHNCXR_0492_1.png', 'CHNCXR_0493_1.png', 'CHNCXR_0494_1.png', 'CHNCXR_0495_1.png', 'CHNCXR_0496_1.png', 'CHNCXR_0497_1.png', 'CHNCXR_0498_1.png', 'CHNCXR_0499_1.png', 'CHNCXR_0500_1.png', 'CHNCXR_0502_1.png', 'CHNCXR_0505_1.png', 'CHNCXR_0560_1.png', 'CHNCXR_0561_1.png', 'CHNCXR_0562_1.png', 'CHNCXR_0563_1.png', 'CHNCXR_0564_1.png', 'CHNCXR_0565_1.png'}
 
 DONUT_USER_HOME = "/nas/home/jaditi"
 # --- Get input files ----------------------------------------------------------
@@ -47,46 +47,98 @@ def train_test_val_split(preprocess, training_input_files, mask_files, processed
     np.random.seed(4)
     process_jobs = [Job(preprocess).add_args("--type", group) for group in ["train", "val", "test"]]
     augmented_masks = []
-    random.shuffle(training_input_files)
 
+
+    # --- Write ReplicaCatalog -----------------------------------------------------
+    rc = ReplicaCatalog()
+
+    # add mask images to rc
+    for f in LUNG_MASK_IMG_DIR.iterdir():
+        if f.name.endswith(".png"):
+            if f.name in IGNORE_IMAGES:
+                continue
+            
+            mask_files.append(File(f.name))
+            rc.add_replica(site="local", lfn=f.name, pfn=f.resolve())
+    
+    #add an empty(probably checkpoint file
+    #checkpoint files  and results (empty one should be given if none exists)
+    for fname in ["inputs/checkpoints/study_checkpoint.pkl", "bin/model/unet.py", "bin/model/utils.py"]:
+        p = Path(__file__).parent.resolve() / fname
+        if not p.exists():
+            with open(p, "w") as dummyFile:
+                dummyFile.write("")
+        replicaFile = File(p.name)
+        rc.add_replica(site="local", lfn=replicaFile, pfn=p)
+
+    for f in LUNG_IMG_DIR.iterdir():
+        if f.name.endswith(".png") and "mask" not in f.name.lower():
+            training_input_files.append(f)
+
+    random.shuffle(training_input_files)
     l = len(training_input_files) if num_inputs == -1 else num_inputs
     print('Length ', l)
-    for i, f in enumerate(training_input_files):
-        if i+1 <= 0.7*l:
-            process_jobs[0].add_inputs(f)
-            op_file1 = File("train_"+f.lfn.replace(".png", "_norm.png"))
-            op_file2 = File("train_"+f.lfn.replace(".png", "_0_norm.png"))
-            op_file3 = File("train_"+f.lfn.replace(".png", "_1_norm.png"))
-            op_mask2 = File(f.lfn.replace(".png", "_0_mask.png"))
-            op_mask3 = File(f.lfn.replace(".png", "_1_mask.png"))
-            for m in mask_files:
-                mname = m.lfn[0:-9]
-                if f.lfn[0:-4] == mname:
-                    training_masks.append(m)
-                    break
-            process_jobs[0].add_outputs(op_file1, op_file2, op_file3, op_mask2, op_mask3)
-            augmented_masks.extend([op_mask2, op_mask3])
-            processed_training_files.extend([op_file1, op_file2, op_file3])
-        elif i+1 <= 0.9*l:
-            process_jobs[1].add_inputs(f)
-            op_file = File("val_"+f.lfn.replace(".png", "_norm.png"))
-            for m in mask_files:
-                mname = m.lfn[0:-9]
-                if f.lfn[0:-4] == mname:
-                    val_masks.append(m)
-                    break
-            process_jobs[1].add_outputs(op_file)
-            processed_val_files.append(op_file)
-        else:
-            process_jobs[2].add_inputs(f)
-            op_file = File("test_"+f.lfn.replace(".png", "_norm.png"))
-            for m in mask_files:
-                
-                mname = m.lfn[0:-9]
-                if f.lfn[0:-4] == mname:
-                    test_masks.append(m)
-            process_jobs[2].add_outputs(op_file)
-            processed_test_files.append(op_file)
+
+    i = 0
+    for file in LUNG_IMG_DIR.iterdir():
+        if file.name not in IGNORE_IMAGES:
+            if i+1 <= 0.7*l:
+                f = File("train_{}".format(file.name))
+                rc.add_replica(site="local", lfn=f, pfn=file.resolve()) 
+
+                process_jobs[0].add_inputs(f)
+                log.info("preprocess_train adding input {}".format(f))
+                op_file1 = File(f.lfn.replace(".png", "_norm.png"))
+                op_file2 = File(f.lfn.replace(".png", "_0_norm.png"))
+                op_file3 = File(f.lfn.replace(".png", "_1_norm.png"))
+                op_mask2 = File(file.name.replace(".png", "_0_mask.png"))
+                op_mask3 = File(file.name.replace(".png", "_1_mask.png"))
+
+                for m in mask_files:
+                    mname = m.lfn[0:-9]
+                    if file.name[0:-4] == mname:
+                        training_masks.append(m)
+                        break
+
+                process_jobs[0].add_outputs(op_file1, op_file2, op_file3, op_mask2, op_mask3)
+                augmented_masks.extend([op_mask2, op_mask3])
+                processed_training_files.extend([op_file1, op_file2, op_file3])
+
+            elif i+1 <= 0.9*l:
+                f = File("val_{}".format(file.name))
+                rc.add_replica(site="local", lfn=f, pfn=file.resolve())
+
+                process_jobs[1].add_inputs(f)
+                log.info("preprocess_val adding input {}".format(f))
+                op_file = File(f.lfn.replace(".png", "_norm.png"))
+                for m in mask_files:
+                    mname = m.lfn[0:-9]
+                    if file.name[0:-4] == mname:
+                        val_masks.append(m)
+                        break
+                    
+                process_jobs[1].add_outputs(op_file)
+                processed_val_files.append(op_file)
+
+            else:
+                f = File("test_{}".format(file.name))
+                rc.add_replica(site="local", lfn=f, pfn=file.resolve())
+
+                process_jobs[2].add_inputs(f)
+                op_file = File(f.lfn.replace(".png", "_norm.png"))
+                for m in mask_files:
+                    mname = m.lfn[0:-9]
+                    if file.name[0:-4] == mname:
+                        test_masks.append(m)
+
+                process_jobs[2].add_outputs(op_file)
+                log.info("preprocess_test adding input {}".format(f))
+                processed_test_files.append(op_file)
+
+            i += 1
+
+    log.info("writing rc with {} files collected from: {}".format(len(training_input_files)+len(mask_files), [LUNG_IMG_DIR, LUNG_MASK_IMG_DIR]))
+    rc.write()
 
     # for preprocess_job in process_jobs:
     #     preprocess_job.add_inputs(*mask_files)
@@ -259,39 +311,13 @@ def run_workflow(args):
         sc = create_site_catalog()
         sc.write()
 
-    # --- Write ReplicaCatalog -----------------------------------------------------
-    training_input_files = []
-    mask_files = []
-
-    rc = ReplicaCatalog()
-
-    for _dir, _list in [
-            (LUNG_IMG_DIR, training_input_files), 
-            (LUNG_MASK_IMG_DIR, mask_files), 
-        ]:
-        for f in _dir.iterdir():
-            if f.name.endswith(".png"):
-                if f.name in IGNORE_IMAGES: continue
-                _list.append(File(f.name))
-                rc.add_replica(site="local", lfn=f.name, pfn=f.resolve())
-
-    #add an empty(probably checkpoint file
-    #checkpoint files  and results (empty one should be given if none exists)
-    for fname in ["inputs/checkpoints/study_checkpoint.pkl", "bin/model/unet.py", "bin/model/utils.py"]:
-        p = Path(__file__).parent.resolve() / fname
-        if not p.exists():
-            with open(p, "w") as dummyFile:
-                dummyFile.write("")
-        replicaFile = File(p.name)
-        rc.add_replica(site="local", lfn=replicaFile, pfn=p)
-
-    log.info("writing rc with {} files collected from: {}".format(len(training_input_files)+len(mask_files), [LUNG_IMG_DIR, LUNG_MASK_IMG_DIR]))
-    rc.write()
-
     # --- Generate and run Workflow ------------------------------------------------
     wf = Workflow("lung-instance-segmentation-wf")
 
     #create preprocess job
+    training_input_files = []
+    mask_files = []
+
     processed_training_files = []
     processed_val_files = []
     processed_test_files = []
@@ -340,13 +366,13 @@ def run_workflow(args):
                     .add_outputs(pdf_analysis)
 
     wf.add_jobs(evaluate_job)
-
+    
     # run workflow
     log.info("begin workflow execution")
     if args.donut:
         wf.plan(submit=True, dir="runs", sites=["donut"], output_sites=["local"], verbose=3)
     else:
-        wf.plan(submit=True, dir="runs", sites=["condorpool"], output_sites=["local"], verbose=3)
+        wf.plan(submit=True, dir="runs", sites=["condorpool"], output_sites=["local"])
 
     #wf.graph(include_files=True, no_simplify=True, label="xform-id", output="graph.dot")
 
