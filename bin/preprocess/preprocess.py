48e3355aeab91bab4e8de53170744d656f58e06c
Ryan Tanaka <tanaka@isi.edu>
2021-08-13 18:25:07 -0700

    updated preproces.py to only readed needed input files; updated preprocess to cleanup its inputs

--- bin/preprocess/preprocess.py
+++ bin/preprocess/preprocess.py
@@ -6,6 +6,7 @@ import cv2
 import numpy as np
 from cv2 import imread
 import random
+from pathlib import Path
 
 CUTOFF = {"train": [0, 0.7], "val": [0.7, 0.9], "test": [0.9, 1]}
 IGNORE_IMAGES = ['CHNCXR_0025_0.png', 'CHNCXR_0036_0.png', 'CHNCXR_0037_0.png', 'CHNCXR_0038_0.png', 'CHNCXR_0039_0.png', 'CHNCXR_0040_0.png', 'CHNCXR_0065_0.png', 'CHNCXR_0181_0.png', 'CHNCXR_0182_0.png', 'CHNCXR_0183_0.png', 'CHNCXR_0184_0.png', 'CHNCXR_0185_0.png', 'CHNCXR_0186_0.png', 'CHNCXR_0187_0.png', 'CHNCXR_0188_0.png', 'CHNCXR_0189_0.png', 'CHNCXR_0190_0.png', 'CHNCXR_0191_0.png', 'CHNCXR_0192_0.png', 'CHNCXR_0193_0.png', 'CHNCXR_0194_0.png', 'CHNCXR_0195_0.png', 'CHNCXR_0196_0.png', 'CHNCXR_0197_0.png', 'CHNCXR_0198_0.png', 'CHNCXR_0199_0.png', 'CHNCXR_0200_0.png', 'CHNCXR_0201_0.png', 'CHNCXR_0202_0.png', 'CHNCXR_0203_0.png', 'CHNCXR_0204_0.png', 'CHNCXR_0205_0.png', 'CHNCXR_0206_0.png', 'CHNCXR_0207_0.png', 'CHNCXR_0208_0.png', 'CHNCXR_0209_0.png', 'CHNCXR_0210_0.png', 'CHNCXR_0211_0.png', 'CHNCXR_0212_0.png', 'CHNCXR_0213_0.png', 'CHNCXR_0214_0.png', 'CHNCXR_0215_0.png', 'CHNCXR_0216_0.png', 'CHNCXR_0217_0.png', 'CHNCXR_0218_0.png', 'CHNCXR_0219_0.png', 'CHNCXR_0220_0.png', 'CHNCXR_0336_1.png', 'CHNCXR_0341_1.png', 'CHNCXR_0342_1.png', 'CHNCXR_0343_1.png', 'CHNCXR_0344_1.png', 'CHNCXR_0345_1.png', 'CHNCXR_0346_1.png', 'CHNCXR_0347_1.png', 'CHNCXR_0348_1.png', 'CHNCXR_0349_1.png', 'CHNCXR_0350_1.png', 'CHNCXR_0351_1.png', 'CHNCXR_0352_1.png', 'CHNCXR_0353_1.png', 'CHNCXR_0354_1.png', 'CHNCXR_0355_1.png', 'CHNCXR_0356_1.png', 'CHNCXR_0357_1.png', 'CHNCXR_0358_1.png', 'CHNCXR_0359_1.png', 'CHNCXR_0360_1.png', 'CHNCXR_0481_1.png', 'CHNCXR_0482_1.png', 'CHNCXR_0483_1.png', 'CHNCXR_0484_1.png', 'CHNCXR_0485_1.png', 'CHNCXR_0486_1.png', 'CHNCXR_0487_1.png', 'CHNCXR_0488_1.png', 'CHNCXR_0489_1.png', 'CHNCXR_0490_1.png', 'CHNCXR_0491_1.png', 'CHNCXR_0492_1.png', 'CHNCXR_0493_1.png', 'CHNCXR_0494_1.png', 'CHNCXR_0495_1.png', 'CHNCXR_0496_1.png', 'CHNCXR_0497_1.png', 'CHNCXR_0498_1.png', 'CHNCXR_0499_1.png', 'CHNCXR_0500_1.png', 'CHNCXR_0502_1.png', 'CHNCXR_0505_1.png', 'CHNCXR_0560_1.png', 'CHNCXR_0561_1.png', 'CHNCXR_0562_1.png', 'CHNCXR_0563_1.png', 'CHNCXR_0564_1.png', 'CHNCXR_0565_1.png']
@@ -109,14 +110,29 @@ if __name__=="__main__":
     all_files_length = len(all_files)
     random.shuffle(all_files)
     lower_limit, upper_limit = CUTOFF[args.type][0]*all_files_length, CUTOFF[args.type][1]*all_files_length
-    for ind, i in enumerate(all_files):
-        if ".png" in i: 
-            if args.cutoff=='t':
-                if ind > upper_limit: break
-                if ind < lower_limit: continue
-            if "mask" in i: all_masks.append(i)
-            else: images.append(i)
-    if args.cutoff=='t': all_masks = os.listdir(args.mask_dir)
+
+    i = 0
+    for f in all_files:
+        if f.endswith(".png"):
+            if "mask" in f:
+                all_masks.append(f)
+            else:
+                # running without pegasus
+                if args.cutoff == "t":
+                    if i > upper_limit:
+                        break
+
+                    if i < lower_limit:
+                        continue  
+
+                    images.append(f)
+                    i += 1
+                elif f.startswith(args.type):
+                    images.append(f)
+                    
+
+    if args.cutoff=='t': 
+        all_masks = os.listdir(args.mask_dir)
 
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
@@ -128,27 +144,44 @@ if __name__=="__main__":
     for i in images: 
         img = i
         normalized_image1 = dp.normalize(i)
-        cv2.imwrite(os.path.join(args.output_dir, args.type+'_'+i[0:-4]+"_norm.png"), normalized_image1)
+        cv2.imwrite(os.path.join(args.output_dir, i[0:-4]+"_norm.png"), normalized_image1)
         #creating transformations for the train images
         if args.type == "train": 
+            if img.startswith("train_"):
+                img = img[6:-4] # train_CHNCXR_0003_0.png -> CHNCXR_0003_0
+            else:
+                img = img[:-4] # CHNCXR_0003_0.png -> CHNCXR_0003_0
+
             mask_img = None 
-            fname = img[:-4]
             for m in all_masks:
-                mname = m[:-9]
-                if fname == mname:
+                mname = m[:-9] # CHNCXR_0149_0_mask.png -> CHNCXR_0149_0
+                if img == mname:
                     mask_img = m
                     break
-            if mask_img is None: print('Not found ', i, fname); continue
+            if mask_img is None: 
+                print('Not found ', i, fname); continue
             mask_path = args.mask_dir if args.mask_dir else args.output_dir 
-            flipped = dp.flipImage(img, DIR, False)
-            cv2.imwrite(os.path.join(args.output_dir, args.type+'_'+i[0:-4]+"_0_norm.png"), flipped)
+            flipped = dp.flipImage(i, DIR, False)
+            cv2.imwrite(os.path.join(args.output_dir, args.type+'_'+ img +"_0_norm.png"), flipped)
             flipped = dp.flipImage(mask_img, mask_path)
             # print('Writing to ', os.path.join(args.output_dir, args.type+'_'+i[0:-4]+"_norm.png"))
-            cv2.imwrite(os.path.join(args.output_dir, i[0:-4]+"_0_mask.png"), flipped)
+            cv2.imwrite(os.path.join(args.output_dir, img + "_0_mask.png"), flipped)
 
             for ind, deg in enumerate([8]):
-                rotated = dp.rotateImage(img, DIR, deg, False)
-                cv2.imwrite(os.path.join(args.output_dir, args.type+'_'+i[0:-4]+"_"+str(ind+1)+"_norm.png"), rotated)
+                rotated = dp.rotateImage(i, DIR, deg, False)
+                cv2.imwrite(os.path.join(args.output_dir, args.type+'_'+ img +"_"+str(ind+1)+"_norm.png"), rotated)
                 # print('Writing to ',i[0:-4]+"_"+str(ind+1)+"_mask.png")
                 rotated = dp.rotateImage(mask_img, mask_path, deg)
-                cv2.imwrite(os.path.join(args.output_dir, i[0:-4]+"_"+str(ind+1)+"_mask.png"), rotated)
+                cv2.imwrite(os.path.join(args.output_dir, img +"_"+str(ind+1)+"_mask.png"), rotated)
+    
+    # in the case of clustering preprocess jobs, we want to cleanup input files ourself
+    print("cleanup")
+    for f in images:
+        print("removing {}".format(f))
+        Path(f).unlink()
+        assert Path(f).exists() == False
+
+    for f in all_masks:
+        print("removing {}".format(f))
+        Path(f).unlink()
+        assert Path(f).exists() == False
