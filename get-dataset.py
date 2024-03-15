77ab8330441e069283933416af8d19e08db58093
George Papadimitriou <georgpap@isi.edu>
2024-03-15 13:27:16 -0700

    fixed fetching the dataset

--- get-dataset.py
+++ get-dataset.py
@@ -1,20 +1,35 @@
 #!/usr/bin/env python3
 import os
 import shutil
+from argparse import ArgumentParser
 
-os.environ['KAGGLE_USERNAME'] = "jain12aditi"
-os.environ['KAGGLE_KEY'] = "3ef0718e1548614d5463458cd451db42"
+def download_dataset(username, key):
+    os.environ['KAGGLE_USERNAME'] = username
+    os.environ['KAGGLE_KEY'] = key
 
 
-import kaggle
-kaggle.api.dataset_download_files('nikhilpandey360/chest-xray-masks-and-labels/download', path='.', unzip=True)
+    import kaggle
+    kaggle.api.dataset_download_files('nikhilpandey360/chest-xray-masks-and-labels', path='.', unzip=True)
 
-shutil.rmtree("Lung Segmentation")
-os.rename("data/Lung Segmentation", "data/LungSegmentation")
+    shutil.rmtree("Lung Segmentation")
+    os.rename("data/Lung Segmentation", "data/LungSegmentation")
 
-DIR = "data/LungSegmentation/masks"
-all_masks = os.listdir(DIR)
-for m in all_masks:
-    if "mask" not in m:
-        new_name = m[0:-4]+"_mask.png"
-        shutil.move(os.path.join(DIR, m), os.path.join(DIR, new_name))
+    DIR = "data/LungSegmentation/masks"
+    all_masks = os.listdir(DIR)
+    for m in all_masks:
+        if "mask" not in m:
+            new_name = m[0:-4]+"_mask.png"
+            shutil.move(os.path.join(DIR, m), os.path.join(DIR, new_name))
+
+def main():
+    parser = ArgumentParser(description="Kaggle dataset downloader")
+    parser.add_argument("--username", "-u", metavar="STR", type=str, help="Kaggle API username", required=True)
+    parser.add_argument("--key", "-k", metavar="STR", type=str, help="Kaggle API key", required=True)
+
+    args = parser.parse_args()
+    
+    download_dataset(args.username, args.key)
+
+
+if __name__ == "__main__":
+    main()
